9dfcc5efb248d092b16bc8869b41298aa72fb559
Jaime A Difo Lorenzo <jaimedifolorenzo@gmail.com>
2023-04-02 19:05:07 +0100

    update

--- src/Pages/ViewIngredient.cshtml.cs
+++ src/Pages/ViewIngredient.cshtml.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using QuickRecipes.WebSite.Services;
 using QuickRecipes.WebSite.Models;
+using System.IO;
+using System.Text;
+using Net.Codecrete.QrCodeGenerator;
+
 
 namespace QuickRecipes.WebSite.Pages
 {
@@ -17,9 +21,14 @@ namespace QuickRecipes.WebSite.Pages
 
         public ViewIngredientsModel(ILogger<ViewIngredientsModel> logger) => _logger = logger;
 
-        public Ingredients ingredient { get; set; } 
+        public string base64svg { get; set; }
+        public Ingredients ingredient { get; set; }
+
         public void OnGet(int id, [FromServices] IWebHostEnvironment env)
         {
+            var qr = QrCode.EncodeText("http://localhost:52042/ViewIngredient?id="+id+"&controller=Home", QrCode.Ecc.Medium);
+            string svg = qr.ToSvgString(4);
+            this.base64svg = Convert.ToBase64String(Encoding.UTF8.GetBytes(svg));
             var ingredient = new JsonFileIngredientService(env).GetIngredients().First(n => n.id == id);
             this.ingredient = ingredient;
         }
